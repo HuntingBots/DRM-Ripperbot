332c9982cf8f3ce000a0ba3246c91ee76c4eb5e4
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2025-07-23 13:33:49 +0530

    Update access_control.py
    Attribute error

--- access_control.py
+++ access_control.py
@@ -1,8 +1,8 @@
 import config
 
-OWNER_ID = config.OWNER_ID
-SUDO_USERS = config.SUDO_USERS
-AUTHORIZED_GROUPS = config.AUTHORIZED_GROUPS
+OWNER_ID = config.Config.OWNER_ID
+SUDO_USERS = config.Config.SUDO_USERS
+AUTHORIZED_GROUPS = config.Config.AUTHORIZED_GROUPS
 
 def is_authorized(user_id: int, chat_id: int) -> bool:
     """Return True if user is OWNER, SUDO, or message is in an authorized group."""
